379c5f0522d7ab7615654b938dc70a70355f1092
EndiIrawan <endiirawan273@gmail.com>
2015-11-03 16:39:19 +0700

    add information for country, language and city

--- lib/searchjoy/track.rb
+++ lib/searchjoy/track.rb
@@ -1,12 +1,12 @@
 module Searchjoy
   module Track
 
-    def search_with_track(term, options = {})
+    def search_with_track(term, options = {}, country, language, city)
       results = search_without_track(term, options)
 
       if options[:track]
         attributes = options[:track] == true ? {} : options[:track]
-        results.search = Searchjoy::Search.create({search_type: self.name, query: term, results_count: results.total_count}.merge(attributes))
+        results.search = Searchjoy::Search.create({search_type: self.name, query: term, results_count: results.total_count, country: country, language: language, city: city}.merge(attributes))
       end
 
       results
