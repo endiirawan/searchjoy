65857de8d173c0f905a11fb23177d8dcb6d52a98
EndiIrawan <endiirawan273@gmail.com>
2015-11-04 10:22:33 +0700

    rename country_iso and city_id

--- lib/searchjoy/track.rb
+++ lib/searchjoy/track.rb
@@ -1,12 +1,12 @@
 module Searchjoy
   module Track
 
-    def search_with_track(term, options = {}, country, language, city)
+    def search_with_track(term, options = {}, country_iso, language, city_id)
       results = search_without_track(term, options)
 
       if options[:track]
         attributes = options[:track] == true ? {} : options[:track]
-        results.search = Searchjoy::Search.create({search_type: self.name, query: term, results_count: results.total_count, country: country, language: language, city: city}.merge(attributes))
+        results.search = Searchjoy::Search.create({search_type: self.name, query: term, results_count: results.total_count, country_iso: country_iso, language: language, city_id: city_id}.merge(attributes))
       end
 
       results
